6b263d02c0145466a335ab9f94f142f7d5ce6c53
Germeen91 <germeen.faried37@gmail.com>
2024-02-06 12:05:50 -0800

    1

--- src/Java11StringDemo/StringDemo11.java
+++ src/Java11StringDemo/StringDemo11.java
@@ -6,7 +6,6 @@ public class StringDemo11 {
 
 
         String str="lkijt sHGDC B#$#*^$@!@123";
-
         System.out.println(str.replaceAll("[a-z]","*"));
         System.out.println(str.replaceAll("[A-Z]","*"));
         System.out.println(str.replaceAll("[0-9]","*"));
